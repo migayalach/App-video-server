f4e493540daf31deb91dfb00e420d828ca99af07
migayalach <ayalachavezmiguel@gmail.com>
2025-01-13 23:41:26 -0400

    bcrypt iniciate and operation hash is ready

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  async create(infoUser: CreateUserDto) {
-    return [infoUser];
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  // SI EL EMAIL YA EXISTE ROMPER
+  // SI PASA ALGO EXTRA ROMPER
+  async create(infoUser: CreateUserDto): Promise<User> {
+    infoUser = {
+      ...infoUser,
+      password: await bcrypt.hash(infoUser.password, 10),
+    };
+    const createUser = new this.userModel(infoUser);
+    return await createUser.save();
   }
 
   findAll() {
