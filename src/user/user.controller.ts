5c5dd369e3dcd573fcc41eb0abc386a4061574db
migayalach <ayalachavezmiguel@gmail.com>
2025-01-13 23:40:37 -0400

    constructor iniciate

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,13 +10,14 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto): Promise<any[]> {
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return await this.userService.create(createUserDto);
   }
 
