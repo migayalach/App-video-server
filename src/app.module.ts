ac10a86a414e8c231e9e9a09290f23a5515ed411
migayalach <ayalachavezmiguel@gmail.com>
2025-01-13 23:39:12 -0400

    In this file imports Mongoose and userModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/appVideo'), UserModule],
   controllers: [],
   providers: [],
 })
