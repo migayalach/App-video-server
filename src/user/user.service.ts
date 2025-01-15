b76d1be9e1ad30b74d1988408c14bbf3b491eea6
migayalach <ayalachavezmiguel@gmail.com>
2025-01-15 09:31:39 -0400

    user service in proccess to the 90% with interfaces

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,12 +5,13 @@ import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
+import { UserResponse, UserData } from 'src/interfaces/user.interface';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async create(infoUser: CreateUserDto): Promise<User> {
+  async create(infoUser: CreateUserDto): Promise<UserResponse> {
     try {
       const existEmail = await this.userModel.findOne({
         email: infoUser.email,
@@ -29,7 +30,12 @@ export class UserService {
         password: await bcrypt.hash(infoUser.password, 10),
       };
       const createUser = new this.userModel(infoUser);
-      return await createUser.save();
+      const { _id } = await createUser.save();
+
+      return {
+        message: 'User created successfully',
+        user: await this.findOne(_id as string),
+      };
     } catch (error) {
       if (error instanceof HttpException) {
         throw error;
@@ -44,19 +50,65 @@ export class UserService {
     }
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll(): Promise<any> {
+    return await this.userModel.find().select('-password');
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(idUser: string): Promise<UserData> {
+    try {
+      const infoUser = await this.userModel
+        .findById(idUser)
+        .select('-password -follow');
+      if (!infoUser) {
+        throw new HttpException(
+          {
+            status: HttpStatus.NOT_FOUND,
+            error: `Sorry, this user don't exists.`,
+          },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return {
+        idUser: infoUser._id.toString(),
+        name: infoUser.name,
+        email: infoUser.email,
+      };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while search the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return { a: `This action updates a #${id} user`, b: updateUserDto };
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(
+    idUser: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserResponse> {
+    try {
+      await this.findOne(idUser);
+      await this.userModel.findByIdAndUpdate(idUser, updateUserDto);
+      return {
+        message: 'User updated successfully',
+        user: await this.findOne(idUser),
+      };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while search the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
