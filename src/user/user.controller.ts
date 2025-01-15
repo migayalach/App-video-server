26a9515264e09f7e9885630ccae0070226cf292d
migayalach <ayalachavezmiguel@gmail.com>
2025-01-15 09:30:18 -0400

    User controller with operations Create, Update, Get all and id

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,43 +1,33 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './schemas/user.schema';
+import { UserResponse, UserData } from 'src/interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+  async create(@Body() createUserDto: CreateUserDto): Promise<UserResponse> {
     return await this.userService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  async findAll(): Promise<any> {
+    return await this.userService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  @Get(':idUser')
+  async findOne(@Param('idUser') idUser: string): Promise<UserData> {
+    return await this.userService.findOne(idUser);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Patch(':idUser')
+  async update(
+    @Param('idUser') idUser: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UserResponse> {
+    return await this.userService.update(idUser, updateUserDto);
   }
 }
