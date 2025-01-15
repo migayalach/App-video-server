aefdfa4a2cb78090dd61952be261be790f209bcf
migayalach <ayalachavezmiguel@gmail.com>
2025-01-15 14:02:49 -0400

    'app.module' configured for use of .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/appVideo'), UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/appVideo'),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
