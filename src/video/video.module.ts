2b1ff643facabb50984ecc988dd526b56448aadb
migayalach <ayalachavezmiguel@gmail.com>
2025-01-16 23:45:15 -0400

    video module

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { VideoController } from './video.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Video, VideoSchema } from './schemas/video.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
+  ],
   controllers: [VideoController],
   providers: [VideoService],
 })
