bf0e74b57d9cadcb73be0b9b75c97184fb614b7e
migayalach <ayalachavezmiguel@gmail.com>
2025-01-16 23:51:35 -0400

    config initiate and ready

--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -1,26 +1,92 @@
 import { Injectable } from '@nestjs/common';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
+import { HttpStatus, HttpException } from '@nestjs/common';
 
 @Injectable()
 export class VideoService {
   async create(infoVideo: CreateVideoDto) {
-    return await 'This action adds a new video';
+    try {
+      return await infoVideo;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while creating the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async findAll() {
-    return await `This action returns all video`;
+    try {
+      return await `This action returns all video`;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while creating the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  async findOne(id: string) {
-    return await `This action returns a #${id} video`;
+  async findOne(idVideo: string) {
+    try {
+      return await `This action returns a #${idVideo} video`;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while creating the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  async update(id: string, infoVideo: UpdateVideoDto) {
-    return await `This action updates a #${id} video`;
+  async update(idVideo: string, infoVideo: UpdateVideoDto) {
+    try {
+      return await infoVideo;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while creating the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  async remove(id: string) {
-    return await `This action removes a #${id} video`;
+  async remove(idVideo: string) {
+    try {
+      return await `This action removes a #${idVideo} video`;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while creating the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
