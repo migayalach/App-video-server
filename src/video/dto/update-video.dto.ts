18b786b5a1cab957343073c6c2e749e48ef2aac5
migayalach <ayalachavezmiguel@gmail.com>
2025-01-16 23:42:43 -0400

    update dto ready to use

--- src/video/dto/update-video.dto.ts
+++ src/video/dto/update-video.dto.ts
@@ -1,4 +1,33 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateVideoDto } from './create-video.dto';
+import {
+  IsEnum,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Types } from 'mongoose';
+import { VideoState } from 'src/enums/video.enum';
 
-export class UpdateVideoDto extends PartialType(CreateVideoDto) {}
+export class UpdateVideoDto extends PartialType(CreateVideoDto) {
+  @IsMongoId()
+  @IsNotEmpty()
+  idUser: Types.ObjectId;
+
+  @IsString()
+  @IsNotEmpty()
+  nameVideo: string;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  url: string;
+
+  @IsEnum(VideoState)
+  @IsNotEmpty()
+  stateVideo: VideoState;
+}
