188c778390c874b69ba7a93ac86949b35240c25e
migayalach <ayalachavezmiguel@gmail.com>
2025-01-17 11:55:43 -0400

    middleware ready

--- src/video/video.middleware.ts
+++ src/video/video.middleware.ts
@@ -5,5 +5,39 @@ export function VideoMiddleware(
   response: Response,
   next: NextFunction,
 ) {
+  const sendError = (message: string) => {
+    response.status(400).json({ status: 400, message });
+  };
+
+  if (request.method === 'POST') {
+    const { idUser, nameVideo, url } = request.body;
+
+    if (!idUser || !idUser.trim().length) {
+      return sendError('El campo "idUser" es obligatorio');
+    }
+    if (!nameVideo || !nameVideo.trim().length) {
+      return sendError('El campo "nameVideo" es obligatorio');
+    }
+    if (!url || !url.trim().length) {
+      return sendError('El campo "url" es obligatorio');
+    }
+  }
+
+  if (request.method === 'PATCH') {
+    const { idUser, nameVideo, url, stateVideo } = request.body;
+    if (!idUser || !idUser.trim().length) {
+      return sendError('El campo "idUser" es obligatorio');
+    }
+    if (!nameVideo || !nameVideo.trim().length) {
+      return sendError('El campo "nameVideo" es obligatorio');
+    }
+    if (!url || !url.trim().length) {
+      return sendError('El campo "url" es obligatorio');
+    }
+    if (stateVideo !== 'true' && stateVideo !== 'false') {
+      return sendError('This state is not valid');
+    }
+  }
+
   next();
 }
