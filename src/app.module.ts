2fca4796a1046965b449a9d36cef5699e4ae3a31
migayalach <ayalachavezmiguel@gmail.com>
2025-01-16 22:50:53 -0400

    video entry point

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,14 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserMiddleware } from './user/user.middleware';
+import { VideoModule } from './video/video.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost/appVideo'),
     UserModule,
+    VideoModule,
   ],
   controllers: [],
   providers: [],
