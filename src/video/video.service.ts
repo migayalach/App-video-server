2019a87d45a4cc4249f7e1d7ea905f2089743223
migayalach <ayalachavezmiguel@gmail.com>
2025-01-17 11:31:29 -0400

    service video with CRUD complete

--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -2,12 +2,39 @@ import { Injectable } from '@nestjs/common';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
 import { HttpStatus, HttpException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Video } from './schemas/video.schema';
+import { Model, Types } from 'mongoose';
+import { clearResVideos, clearVideoRes } from 'src/utils/clearResponse.util';
+import { response } from 'src/utils/response.util';
 
 @Injectable()
 export class VideoService {
+  constructor(
+    @InjectModel(Video.name) private videoModel: Model<Video>,
+    private useSevice: UserService,
+  ) {}
   async create(infoVideo: CreateVideoDto) {
     try {
-      return await infoVideo;
+      await this.useSevice.findOne(infoVideo.idUser.toString());
+      if (await this.videoModel.findOne({ nameVideo: infoVideo.nameVideo })) {
+        throw new HttpException(
+          {
+            status: HttpStatus.NOT_FOUND,
+            error: `Sorry this name currently exists`,
+          },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      const createVideo = new this.videoModel({
+        ...infoVideo,
+        idUser: new Types.ObjectId(infoVideo.idUser),
+      });
+      return {
+        message: 'Video created successfully',
+        video: clearVideoRes(await createVideo.save()),
+      };
     } catch (error) {
       if (error instanceof HttpException) {
         throw error;
@@ -22,9 +49,16 @@ export class VideoService {
     }
   }
 
-  async findAll() {
+  async findAll(page?: number) {
     try {
-      return await `This action returns all video`;
+      const results = clearResVideos(
+        await this.videoModel.find().select('-__v'),
+      );
+      if (!page) {
+        page = 1;
+      }
+
+      return response(results, page, 'video?');
     } catch (error) {
       if (error instanceof HttpException) {
         throw error;
@@ -41,7 +75,17 @@ export class VideoService {
 
   async findOne(idVideo: string) {
     try {
-      return await `This action returns a #${idVideo} video`;
+      const videoInfo = await this.videoModel.findById(idVideo).select('-__v');
+      if (!videoInfo) {
+        throw new HttpException(
+          {
+            status: HttpStatus.NOT_FOUND,
+            message: "Sorry this video don't exist",
+          },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return clearVideoRes(videoInfo);
     } catch (error) {
       if (error instanceof HttpException) {
         throw error;
@@ -58,7 +102,22 @@ export class VideoService {
 
   async update(idVideo: string, infoVideo: UpdateVideoDto) {
     try {
-      return await infoVideo;
+      await this.findOne(idVideo);
+      const updateVideo = {
+        ...infoVideo,
+        idUser: new Types.ObjectId(infoVideo.idUser),
+      };
+      const videoInfo = await this.videoModel.findByIdAndUpdate(
+        idVideo,
+        updateVideo,
+        {
+          new: true,
+        },
+      );
+      return {
+        message: `Video updated successfully`,
+        video: clearVideoRes(videoInfo),
+      };
     } catch (error) {
       if (error instanceof HttpException) {
         throw error;
@@ -66,7 +125,7 @@ export class VideoService {
       throw new HttpException(
         {
           status: HttpStatus.INTERNAL_SERVER_ERROR,
-          error: 'An unexpected error occurred while creating the user.',
+          error: 'An unexpected error occurred while creating the video.',
         },
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
@@ -75,7 +134,11 @@ export class VideoService {
 
   async remove(idVideo: string) {
     try {
-      return await `This action removes a #${idVideo} video`;
+      await this.findOne(idVideo);
+      return {
+        message: `Video deleted successfully`,
+        video: clearVideoRes(await this.videoModel.findByIdAndDelete(idVideo)),
+      };
     } catch (error) {
       if (error instanceof HttpException) {
         throw error;
