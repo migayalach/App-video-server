5868bf6f851d3a2fb5ff513524b1d441c6683802
migayalach <ayalachavezmiguel@gmail.com>
2025-01-16 23:42:00 -0400

    create dto ready to use

--- src/video/dto/create-video.dto.ts
+++ src/video/dto/create-video.dto.ts
@@ -1,1 +1,20 @@
-export class CreateVideoDto {}
+import { IsMongoId, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { Types } from 'mongoose';
+
+export class CreateVideoDto {
+  @IsMongoId()
+  @IsNotEmpty()
+  idUser: Types.ObjectId;
+
+  @IsString()
+  @IsNotEmpty()
+  nameVideo: string;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  url: string;
+}
