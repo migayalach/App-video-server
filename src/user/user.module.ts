f2714ae2c3750e6964a61456929733e2578b2841
migayalach <ayalachavezmiguel@gmail.com>
2025-01-17 11:27:29 -0400

    In user.module I export the 'UserService' for the poster use in other modules

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -8,6 +8,7 @@ import { User, UserSchema } from './schemas/user.schema';
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
+  exports: [UserService],
   controllers: [UserController],
   providers: [UserService],
 })
