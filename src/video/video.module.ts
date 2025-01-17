aa6471e21180554c2057f9ea5476bd8198d49bb7
migayalach <ayalachavezmiguel@gmail.com>
2025-01-17 11:30:47 -0400

    In 'video.module' I imports UserModule for request to verification

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -3,9 +3,11 @@ import { VideoService } from './video.service';
 import { VideoController } from './video.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Video, VideoSchema } from './schemas/video.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
+    UserModule,
     MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
   ],
   controllers: [VideoController],
