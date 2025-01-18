cb158091e47009d74260092fb2e53cfb9d1f42be
migayalach <ayalachavezmiguel@gmail.com>
2025-01-18 12:23:56 -0400

    like module import user and video models

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -3,10 +3,13 @@ import { LikeService } from './like.service';
 import { LikeController } from './like.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Like, LikeSchema } from './schemas/like.schema';
+import { UserModule } from 'src/user/user.module';
+import { VideoModule } from 'src/video/video.module';
 
 @Module({
   imports: [
-    LikeModule,
+    UserModule,
+    VideoModule,
     MongooseModule.forFeature([{ name: Like.name, schema: LikeSchema }]),
   ],
   controllers: [LikeController],
