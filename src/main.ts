cff614f8aeeff7f3401be63f21396378ffb6c71e
migayalach <ayalachavezmiguel@gmail.com>
2025-01-18 19:36:53 -0400

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Server video')
+    .setDescription('The server video API')
+    .setVersion('1.0')
+    .addTag('Video server documentation')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('app-video', app, documentFactory);
   app.setGlobalPrefix('app-video');
   app.enableCors();
   app.useGlobalPipes(
