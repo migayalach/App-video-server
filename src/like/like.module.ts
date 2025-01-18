b21aa7bb97352459992935a931ea58c8b9dd866d
migayalach <ayalachavezmiguel@gmail.com>
2025-01-17 23:07:35 -0400

    import likeModule for use in the database

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { LikeController } from './like.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Like, LikeSchema } from './schemas/like.schema';
 
 @Module({
+  imports: [
+    LikeModule,
+    MongooseModule.forFeature([{ name: Like.name, schema: LikeSchema }]),
+  ],
   controllers: [LikeController],
   providers: [LikeService],
 })
