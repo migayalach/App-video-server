7cf8f3dd958a5a38a56f0dd217e01743f953d59a
migayalach <ayalachavezmiguel@gmail.com>
2025-01-18 11:50:25 -0400

    created like.dto

--- src/like/dto/create-like.dto.ts
+++ src/like/dto/create-like.dto.ts
@@ -1,1 +1,12 @@
-export class CreateLikeDto {}
+import { IsMongoId, IsNotEmpty } from 'class-validator';
+import { Types } from 'mongoose';
+
+export class CreateLikeDto {
+  @IsMongoId()
+  @IsNotEmpty()
+  idUser: Types.ObjectId;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  idVideo: Types.ObjectId;
+}
