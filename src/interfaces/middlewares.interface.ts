966ed4ff5063de3d0bfca20d914ed25314a7146f
migayalach <ayalachavezmiguel@gmail.com>
2025-01-18 23:36:02 -0400

    interface middleware ready

--- src/interfaces/middlewares.interface.ts
+++ src/interfaces/middlewares.interface.ts
@@ -1,5 +1,20 @@
+import { VideoState } from 'src/enums/video.enum';
+
 export interface UserRequestBody {
   name: string;
   email: string;
   password?: string;
+}
+
+export interface VideoReqBodyPost {
+  idUser: string;
+  nameVideo: string;
+  url: string;
+}
+
+export interface VideoReqBodyPatch {
+  idUser: string;
+  nameVideo: string;
+  url: string;
+  stateVideo: VideoState;
 }
