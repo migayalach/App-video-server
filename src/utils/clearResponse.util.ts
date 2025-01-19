560479a8d707b840200424caf66bc9ae4768699c
migayalach <ayalachavezmiguel@gmail.com>
2025-01-19 12:15:34 -0400

    typing ready in clearResponse

--- src/utils/clearResponse.util.ts
+++ src/utils/clearResponse.util.ts
@@ -1,30 +1,31 @@
-export const clearVideoRes = (infoVideo) => {
+import { VideoInfo, VideoResponseClear } from 'src/interfaces/video.interface';
+
+export const clearVideoRes = ({
+  _id,
+  idUser,
+  nameVideo,
+  description,
+  url,
+  stateVideo,
+  dateCreate,
+}): VideoResponseClear => {
   return {
-    idVideo: infoVideo._id,
-    idUser: infoVideo.idUser,
-    nameVideo: infoVideo.nameVideo,
-    description: infoVideo.description,
-    url: infoVideo.url,
-    stateVideo: infoVideo.stateVideo,
-    dateCreate: infoVideo.dateCreate,
+    idVideo: _id,
+    idUser: idUser,
+    nameVideo: nameVideo,
+    description: description,
+    url: url,
+    stateVideo: stateVideo,
+    dateCreate: dateCreate,
   };
 };
 
-export const clearResVideos = (results) => {
+export const clearResVideos = (
+  results: Array<VideoInfo>,
+): Array<VideoResponseClear> => {
   const data = [];
   for (let index = 0; index < results.length; index++) {
     data.push(clearVideoRes(results[index]));
-  }
-  return data;
-};
-
-export const clearListVideo = (list) => {
-  const data = [];
-  for (let index = 0; index < list.length; index++) {
-    const obj = { idLike: '', video: {} };
-    obj.idLike = list[index]._id;
-    obj.video = clearVideoRes(list[index].idVideo);
-    data.push(obj);
   }
   return data;
 };
