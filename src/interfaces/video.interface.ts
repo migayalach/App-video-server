dc6247b78d2ce29293125e32da4c97a393f44f2c
migayalach <ayalachavezmiguel@gmail.com>
2025-01-19 12:13:22 -0400

    typing ready for video

--- src/interfaces/video.interface.ts
+++ src/interfaces/video.interface.ts
@@ -15,3 +15,23 @@ export interface VideoData {
   stateVideo: VideoState;
   dateCreate: Date;
 }
+
+export interface VideoInfo {
+  _id: string | ObjectId;
+  idUser: string | ObjectId;
+  nameVideo: string;
+  description: string;
+  url: string;
+  stateVideo: VideoState;
+  dateCreate: Date;
+}
+
+export interface VideoResponseClear {
+  idVideo: string;
+  idUser: string;
+  nameVideo: string;
+  description: string;
+  url: string;
+  stateVideo: VideoState;
+  dateCreate: Date;
+}
