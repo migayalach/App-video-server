36e86264812b1300ef4b0c52563745287a2cc0fc
migayalach <ayalachavezmiguel@gmail.com>
2025-01-18 23:23:37 -0400

    typing complete for standaritation response the server

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -11,23 +11,25 @@ import {
 import { VideoService } from './video.service';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
+import { VideoData, VideoResponse } from 'src/interfaces/video.interface';
+import { Response } from 'src/interfaces/response.interface';
 
 @Controller('video')
 export class VideoController {
   constructor(private readonly videoService: VideoService) {}
 
   @Post()
-  async create(@Body() infoVideo: CreateVideoDto): Promise<any> {
+  async create(@Body() infoVideo: CreateVideoDto): Promise<VideoResponse> {
     return await this.videoService.create(infoVideo);
   }
 
   @Get()
-  async findAll(@Query('page') page: string): Promise<any> {
+  async findAll(@Query('page') page: string): Promise<Response> {
     return await this.videoService.findAll(+page);
   }
 
   @Get(':idVideo')
-  async findOne(@Param('idVideo') idVideo: string): Promise<any> {
+  async findOne(@Param('idVideo') idVideo: string): Promise<VideoData> {
     return await this.videoService.findOne(idVideo);
   }
 
@@ -35,12 +37,12 @@ export class VideoController {
   async update(
     @Param('idVideo') idVideo: string,
     @Body() infoVideo: UpdateVideoDto,
-  ): Promise<any> {
+  ): Promise<VideoResponse> {
     return await this.videoService.update(idVideo, infoVideo);
   }
 
   @Delete(':idVideo')
-  async remove(@Param('idVideo') idVideo: string): Promise<any> {
+  async remove(@Param('idVideo') idVideo: string): Promise<VideoResponse> {
     return await this.videoService.remove(idVideo);
   }
 }
