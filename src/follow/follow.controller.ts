881a1192f466735e23694719efa116303abae9f4
migayalach <ayalachavezmiguel@gmail.com>
2025-01-21 12:45:11 -0400

    entry points ready to use

--- src/follow/follow.controller.ts
+++ src/follow/follow.controller.ts
@@ -1,34 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Param, Get, Query } from '@nestjs/common';
 import { FollowService } from './follow.service';
 import { CreateFollowDto } from './dto/create-follow.dto';
-import { UpdateFollowDto } from './dto/update-follow.dto';
 
 @Controller('follow')
 export class FollowController {
   constructor(private readonly followService: FollowService) {}
 
+  @Get(':idUser')
+  async findAll(@Param('idUser') idUser: string, @Query('page') page: string) {
+    return await this.followService.findAll(idUser, +page);
+  }
+
   @Post()
-  create(@Body() createFollowDto: CreateFollowDto) {
-    return this.followService.create(createFollowDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.followService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.followService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFollowDto: UpdateFollowDto) {
-    return this.followService.update(+id, updateFollowDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.followService.remove(+id);
+  async create(@Body() dataFollow: CreateFollowDto): Promise<any> {
+    return await this.followService.create(dataFollow);
   }
 }
