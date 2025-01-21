fd8575f9e45a32abfd69dcdc54498c0908c24b1b
migayalach <ayalachavezmiguel@gmail.com>
2025-01-21 12:44:38 -0400

    Rename this file

--- src/follow/dto/create-follow.dto.ts
+++ src/follow/dto/create-follow.dto.ts
@@ -1,1 +1,17 @@
-export class CreateFollowDto {}
+import { IsEnum, IsMongoId, IsNotEmpty } from 'class-validator';
+import { Types } from 'mongoose';
+import { Follow } from 'src/enums/follow.enum';
+
+export class CreateFollowDto {
+  @IsMongoId()
+  @IsNotEmpty()
+  idUser: Types.ObjectId;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  idCreador: Types.ObjectId;
+
+  @IsNotEmpty()
+  @IsEnum(Follow)
+  option: Follow;
+}
