e8e273681376d94387fa6d1332c2be3befa5afd3
migayalach <ayalachavezmiguel@gmail.com>
2025-01-21 12:46:14 -0400

    added new import 'usemodule' for use method this file

--- src/follow/follow.module.ts
+++ src/follow/follow.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FollowService } from './follow.service';
 import { FollowController } from './follow.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/user/schemas/user.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [
+    UserModule,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [FollowController],
   providers: [FollowService],
 })
