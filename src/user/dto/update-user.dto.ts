29896296187b49f7a30a65a2860ee36452772c53
migayalach <ayalachavezmiguel@gmail.com>
2025-01-20 20:16:05 -0400

    documentation with swagger ready

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,12 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({ description: 'The name of the user', example: 'Nicolas' })
   @IsString()
   @IsOptional()
   name: string;
 
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'nicolasMiau@example.com',
+  })
   @IsEmail()
   @IsOptional()
   email: string;
