4f66de1760d3f5ae3e751a4b82bbaab7f68ffa51
migayalach <ayalachavezmiguel@gmail.com>
2025-01-20 20:15:34 -0400

    clear response

--- src/utils/clearResponse.util.ts
+++ src/utils/clearResponse.util.ts
@@ -1,3 +1,4 @@
+import { LikeResponseGetAll } from 'src/interfaces/like.interface';
 import { VideoInfo, VideoResponseClear } from 'src/interfaces/video.interface';
 
 export const clearVideoRes = ({
@@ -10,8 +11,8 @@ export const clearVideoRes = ({
   dateCreate,
 }): VideoResponseClear => {
   return {
-    idVideo: _id,
-    idUser: idUser,
+    idVideo: _id?.toString(),
+    idUser: idUser?.toString(),
     nameVideo: nameVideo,
     description: description,
     url: url,
@@ -26,6 +27,19 @@ export const clearResVideos = (
   const data = [];
   for (let index = 0; index < results.length; index++) {
     data.push(clearVideoRes(results[index]));
+  }
+  return data;
+};
+
+export const clearListVideoLike = (
+  list: Array<any>,
+): Array<LikeResponseGetAll> => {
+  const data = [];
+  for (let index = 0; index < list.length; index++) {
+    const obj = { idLike: '', video: {} };
+    obj.idLike = list[index]._id;
+    obj.video = clearVideoRes(list[index].idVideo);
+    data.push(obj);
   }
   return data;
 };
