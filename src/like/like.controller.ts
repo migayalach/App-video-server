0f2dda3f9bfa04b3df64ea85371c9638cbd89d87
migayalach <ayalachavezmiguel@gmail.com>
2025-01-20 20:10:38 -0400

    Operation complete

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -9,13 +9,15 @@ import {
 } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { CreateLikeDto } from './dto/create-like.dto';
+import { VideoResponse } from 'src/interfaces/video.interface';
+import { Response } from 'src/interfaces/response.interface';
 
 @Controller('like')
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
 
   @Post()
-  async create(@Body() infoLike: CreateLikeDto): Promise<any> {
+  async create(@Body() infoLike: CreateLikeDto): Promise<VideoResponse> {
     return await this.likeService.create(infoLike);
   }
 
@@ -23,12 +25,12 @@ export class LikeController {
   async findOne(
     @Param('idUser') idUser: string,
     @Query('page') page: string,
-  ): Promise<any> {
+  ): Promise<Response> {
     return await this.likeService.findOne(idUser, +page);
   }
 
   @Delete(':idLike')
-  async remove(@Param('idLike') idLike: string): Promise<any> {
+  async remove(@Param('idLike') idLike: string): Promise<VideoResponse> {
     return await this.likeService.remove(idLike);
   }
 }
