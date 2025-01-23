269b9a9c473c71e9c6a2e5453da82ba9577b6f35
migayalach <ayalachavezmiguel@gmail.com>
2025-01-22 21:58:14 -0400

    documentation with swagger ready

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -11,17 +11,160 @@ import { LikeService } from './like.service';
 import { CreateLikeDto } from './dto/create-like.dto';
 import { VideoResponse } from 'src/interfaces/video.interface';
 import { Response } from 'src/interfaces/response.interface';
+import { ApiOperation, ApiParam, ApiQuery, ApiResponse } from '@nestjs/swagger';
 
 @Controller('like')
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
 
+  //!CREATE LIKE
   @Post()
+  @ApiOperation({ summary: 'Create new like video' })
+  @ApiResponse({
+    status: 201,
+    description: 'Like successfully created',
+    schema: {
+      example: {
+        message: 'Video added to favorites list.',
+        video: {
+          idVideo: '678d123a954eefc016e2ccea',
+          idUser: '6787c185d67c1c00e1758fa8',
+          nameVideo:
+            'Rafael arruina los planes de Rubén | Hasta que la plata nos separe 2006',
+          description:
+            'La oferta presentada por Rafael convence a Jorge y él acepta venderle su hacienda. Rubén y Rodrigo no entienden qué pasa, pues de la nada, quedó por el suelo su sueño de quedarse con una propiedad tan valiosa.',
+          url: 'https://www.youtube.com/watch?v=_Q5H8mt47IM',
+          stateVideo: 'true',
+          dateCreate: '2025-01-19T14:54:50.737Z',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Resource not found',
+    content: {
+      'application/json': {
+        examples: {
+          userNotFound: {
+            summary: 'User not found',
+            value: { message: "Sorry, this user doesn't exist" },
+          },
+          videoRepeat: {
+            summary: 'Video repeat exists',
+            value: {
+              message: 'Sorry this video is curretnly in your favorites',
+            },
+          },
+          videoConflict: {
+            summary: "Video doens't exists",
+            value: {
+              message: "Sorry, this video doesn't exists.",
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'An unexpected error occurred while searching for the like.',
+  })
   async create(@Body() infoLike: CreateLikeDto): Promise<VideoResponse> {
     return await this.likeService.create(infoLike);
   }
 
+  //!GET LIST OF VIDEOS USERS LIKE
   @Get(':idUser')
+  @ApiOperation({ summary: 'Get the list like of users videos' })
+  @ApiParam({
+    name: 'idUser',
+    description: "'idUser' parameter to search the database and get all data.",
+    type: String,
+    example: '6787c164d67c1c00e1758f98',
+  })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    description: 'Page number for pagination',
+    type: Number,
+    example: 1,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User like videos pagination and response',
+    schema: {
+      example: {
+        info: {
+          count: 4,
+          pages: 1,
+          next: null,
+          prev: null,
+        },
+        results: [
+          {
+            idLike: '678bcced664028dc5714ad32',
+            video: {
+              idUser: '678726a888a21c3097b5c5d1',
+              nameVideo:
+                'JACOB FATU TRACIONARÁ a SOLO para ser JEFE TRIBAL? 😨 - ALTOS y BAJOS SMACKDOWN',
+              description:
+                'Conviértete en MIEMBRO y se parte de los SEXY BOYS del Canal 🥰',
+              url: 'https://www.youtube.com/watch?v=b3V4cddqzUI',
+              stateVideo: 'true',
+              dateCreate: '2025-01-18T14:20:23.258Z',
+            },
+          },
+          {
+            idLike: '678d8f8ac2c67b9fca4d5e58',
+            video: {
+              idUser: '678726a888a21c3097b5c5d1',
+              nameVideo: 'My video',
+              description: 'Holis mundo',
+              url: 'https://www.youtube.com/watch?v=L2UA8aUmw4k',
+              stateVideo: 'true',
+              dateCreate: '2025-01-17T14:48:26.821Z',
+            },
+          },
+          {
+            idLike: '678d8fa4c2c67b9fca4d5e64',
+            video: {
+              idUser: '6787c185d67c1c00e1758fa8',
+              nameVideo:
+                'Canción del episodio 5 del Juego del Calamar 2 | Shin Hae Chul - To You (그대에게) | Letra en Español',
+              description:
+                "Pentatlon juego del calamar 2, episodio 5, juego 2, segundo juego del juego del calamar 2, español, letra, lyrics, sub español, Canción del episodio 6 del Juego del Calamar 2 🦑 | 둥글게 둥글게 | Letra en Español letra en español, Squid Game 2 Episode 6 Mingle Game Song Extended, Mingle Game Song With Lyrics | Round and Round | Full Version Soundtrack | Squid Game | Season 2, Korean Children's Songs – 둥글게 둥글게",
+              url: 'https://www.youtube.com/watch?v=018i4yOFf-s',
+              stateVideo: 'true',
+              dateCreate: '2025-01-19T02:43:03.565Z',
+            },
+          },
+          {
+            idLike: '67911e50f9e6f949262708d5',
+            video: {
+              idUser: '6787c185d67c1c00e1758fa8',
+              nameVideo:
+                'Rafael arruina los planes de Rubén | Hasta que la plata nos separe 2006',
+              description:
+                'La oferta presentada por Rafael convence a Jorge y él acepta venderle su hacienda. Rubén y Rodrigo no entienden qué pasa, pues de la nada, quedó por el suelo su sueño de quedarse con una propiedad tan valiosa.',
+              url: 'https://www.youtube.com/watch?v=_Q5H8mt47IM',
+              stateVideo: 'true',
+              dateCreate: '2025-01-19T14:54:50.737Z',
+            },
+          },
+        ],
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: "Sorry, this user doesn't exists.",
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'An unexpected error occurred while searching for the like videos user.',
+  })
   async findOne(
     @Param('idUser') idUser: string,
     @Query('page') page: string,
@@ -29,7 +172,44 @@ export class LikeController {
     return await this.likeService.findOne(idUser, +page);
   }
 
+  //!DELETE LIKE
   @Delete(':idLike')
+  @ApiOperation({ summary: 'Delete like video of list the user' })
+  @ApiParam({
+    name: 'idLike',
+    description:
+      "'idLike' parameter to search the database and get data and delete video.",
+    type: String,
+    example: '67911e50f9e6f949262708d5',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Video successfully delete',
+    schema: {
+      example: {
+        message: 'Video deleted successfully',
+        video: {
+          idVideo: '678d123a954eefc016e2ccea',
+          idUser: '6787c185d67c1c00e1758fa8',
+          nameVideo:
+            'Rafael arruina los planes de Rubén | Hasta que la plata nos separe 2006',
+          description:
+            'La oferta presentada por Rafael convence a Jorge y él acepta venderle su hacienda. Rubén y Rodrigo no entienden qué pasa, pues de la nada, quedó por el suelo su sueño de quedarse con una propiedad tan valiosa.',
+          url: 'https://www.youtube.com/watch?v=_Q5H8mt47IM',
+          stateVideo: 'true',
+          dateCreate: '2025-01-19T14:54:50.737Z',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: "Sorry, this video doesn't exists.",
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'An unexpected error occurred while searching for the video.',
+  })
   async remove(@Param('idLike') idLike: string): Promise<VideoResponse> {
     return await this.likeService.remove(idLike);
   }
