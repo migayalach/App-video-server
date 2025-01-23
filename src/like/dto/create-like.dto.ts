cc6a7aec5116effbd9e8532a343b84b9cb40c828
migayalach <ayalachavezmiguel@gmail.com>
2025-01-22 21:57:50 -0400

    documentation with swagger ready

--- src/like/dto/create-like.dto.ts
+++ src/like/dto/create-like.dto.ts
@@ -1,11 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsMongoId, IsNotEmpty } from 'class-validator';
 import { Types } from 'mongoose';
 
 export class CreateLikeDto {
+  @ApiProperty({
+    description: 'Unique identifier of the user who liked the video.',
+    example: '6787c164d67c1c00e1758f98',
+    type: String,
+  })
   @IsMongoId()
   @IsNotEmpty()
   idUser: Types.ObjectId;
 
+  @ApiProperty({
+    description: 'Unique identifier of the video that was liked.',
+    example: '678d123a954eefc016e2ccea',
+    type: String,
+  })
   @IsMongoId()
   @IsNotEmpty()
   idVideo: Types.ObjectId;
