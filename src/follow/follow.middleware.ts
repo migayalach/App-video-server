11b30fc6ad85cafc293ffeeabb900644d78293cb
migayalach <ayalachavezmiguel@gmail.com>
2025-01-23 13:40:15 -0400

    follow middleware ready

--- src/follow/follow.middleware.ts
+++ src/follow/follow.middleware.ts
@@ -1,8 +1,32 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { Follow } from 'src/enums/follow.enum';
+import { FollowReqBody } from 'src/interfaces/middlewares.interface';
 
-@Injectable()
-export class FollowMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
-    next();
+export function FollowMiddleware(
+  request: Request<object, object, FollowReqBody>,
+  response: Response,
+  next: NextFunction,
+) {
+  const sendError = (message: string) => {
+    response.status(400).json({ status: 400, message });
+  };
+
+  if (request.method === 'POST') {
+    const { idUser, idCreador, option } = request.body;
+    if (!idUser || !idUser.trim().length) {
+      return sendError('El campo "idUser" es obligatorio');
+    }
+    if (!idCreador || !idCreador.trim().length) {
+      return sendError('El campo "idCreator" es obligatorio');
+    }
+    if (
+      !option ||
+      !option.trim().length ||
+      (option !== Follow.Adding && option !== Follow.Delete)
+    ) {
+      return sendError('El campo "option" es obligatorio');
+    }
   }
+
+  next();
 }
