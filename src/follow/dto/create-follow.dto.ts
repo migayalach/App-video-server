3b9506241a4acd6eba511ebc4982ad8fef198eee
migayalach <ayalachavezmiguel@gmail.com>
2025-01-22 21:57:25 -0400

    documentation with swagger ready

--- src/follow/dto/create-follow.dto.ts
+++ src/follow/dto/create-follow.dto.ts
@@ -1,16 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsMongoId, IsNotEmpty } from 'class-validator';
 import { Types } from 'mongoose';
 import { Follow } from 'src/enums/follow.enum';
 
 export class CreateFollowDto {
+  @ApiProperty({
+    description:
+      'Unique identifier of the user who is performing the follow action.',
+    example: '6787c164d67c1c00e1758f98',
+    type: String,
+  })
   @IsMongoId()
   @IsNotEmpty()
   idUser: Types.ObjectId;
 
+  @ApiProperty({
+    description:
+      'Unique identifier of the creator being followed or unfollowed.',
+    example: '6787c164d67c1c00e1758f98',
+    type: String,
+  })
   @IsMongoId()
   @IsNotEmpty()
   idCreador: Types.ObjectId;
 
+  @ApiProperty({
+    description:
+      'Action to perform: "Adding" to follow or "Delete" to unfollow.',
+    examples: {
+      follow: {
+        summary: 'Follow a creator',
+        value: 'Adding',
+      },
+      unfollow: {
+        summary: 'Unfollow a creator',
+        value: 'Delete',
+      },
+    },
+    enum: Follow,
+  })
   @IsNotEmpty()
   @IsEnum(Follow)
   option: Follow;
