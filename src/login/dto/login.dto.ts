6cf54b1505dca4014d59169f249e9cc1e089bff0
migayalach <ayalachavezmiguel@gmail.com>
2025-01-23 22:31:02 -0400

    login.dto create with success with two properties

--- src/login/dto/login.dto.ts
+++ src/login/dto/login.dto.ts
@@ -1,1 +1,12 @@
-export class LoginDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
