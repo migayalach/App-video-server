26a0eb8d35b63c08329a5a075ef0f4fe48de406f
migayalach <ayalachavezmiguel@gmail.com>
2025-01-23 22:35:26 -0400

    middleware ready

--- src/login/login.middleware.ts
+++ src/login/login.middleware.ts
@@ -1,9 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
+import { LoginData } from 'src/interfaces/middlewares.interface';
 
 export function LoginMiddleware(
-  request: Request<object, object, object>,
+  request: Request<object, object, LoginData>,
   response: Response,
   next: NextFunction,
 ) {
+  const sendError = (message: string) => {
+    response.status(400).json({ status: 400, message });
+  };
+  if (request.method === 'POST') {
+    const { email, password } = request.body;
+    if (!email || !email.trim().length) {
+      return sendError('El campo "email" es obligatorio');
+    }
+    if (!password || !password.trim().length) {
+      return sendError('El campo "password" es obligatorio');
+    }
+  }
+
   next();
 }
