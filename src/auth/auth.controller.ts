2686f27094d5713b8982efb08c2b439f322395da
migayalach <ayalachavezmiguel@gmail.com>
2025-01-24 13:47:46 -0400

    created new entry point with added new route 'login'

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async signIn(@Body() infoLogin: LoginDto) {
+    return await this.authService.signIn(infoLogin);
+  }
+}
