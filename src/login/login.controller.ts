593fd3c99621d37e306c98947db641976f0d7f8a
migayalach <ayalachavezmiguel@gmail.com>
2025-01-23 22:34:26 -0400

    login controller added promise of response

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { LoginDto } from './dto/login.dto';
+import { LoginResponse } from 'src/interfaces/login.interface';
 
 @Controller('login')
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
 
   @Post()
-  async create(@Body() infoLogin: LoginDto): Promise<any> {
+  async create(@Body() infoLogin: LoginDto): Promise<LoginResponse> {
     return await this.loginService.create(infoLogin);
   }
 }
