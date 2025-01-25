c6282746e06e72aac37a86d8b1c4446a7dc9b5c6
migayalach <ayalachavezmiguel@gmail.com>
2025-01-25 12:08:38 -0400

    added JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,6 +11,7 @@ import {
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { AuthGuard } from './auth.guard';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -18,6 +19,46 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
+  @ApiOperation({ summary: 'Account Access' })
+  @ApiResponse({
+    status: 201,
+    description: 'Access successfully',
+    schema: {
+      example: {
+        access: true,
+        idUser: '678e730e45b56f4dad365a3c',
+        nameUser: 'Nicolas',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Resource not found',
+    content: {
+      'application/json': {
+        examples: {
+          userNotFound: {
+            summary: 'Email is not valid.',
+            value: {
+              statusCode: 404,
+              message: 'Sorry, the email is not valid.',
+            },
+          },
+          nameConflict: {
+            summary: 'Password is not valid.',
+            value: {
+              statusCode: 404,
+              message: 'Sorry, the password is not valid.',
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'An unexpected error occurred while searching for the user.',
+  })
   async signIn(@Body() infoLogin: LoginDto) {
     return await this.authService.signIn(infoLogin);
   }
