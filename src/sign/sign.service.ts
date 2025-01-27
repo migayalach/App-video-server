9c5dc6bcb3dea695d83a17bb36e1a64cb6d1c581
migayalach <ayalachavezmiguel@gmail.com>
2025-01-27 16:55:25 -0400

    added new methos delete

--- src/sign/sign.service.ts
+++ src/sign/sign.service.ts
@@ -1,14 +1,63 @@
-import { Injectable } from '@nestjs/common';
-import { CreateSignDto } from './dto/create-sign.dto';
-import { UpdateSignDto } from './dto/update-sign.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { SignUpDto } from './dto/signUp.dto';
+import { SignInDto } from './dto/signIn.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class SignService {
-  create(createSignDto: CreateSignDto) {
-    return 'sign up';
+  constructor(private userService: UserService) {}
+
+  async create(infoUser: SignUpDto) {
+    try {
+      return await this.userService.create(infoUser);
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while creating the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  update(updateSignDto: UpdateSignDto) {
-    return `sign in`;
+  async update(infoUser: SignInDto) {
+    try {
+      return await this.userService.emailExist(
+        infoUser.email,
+        infoUser.password,
+      );
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while search the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async delete(idUser: string) {
+    try {
+      return await this.userService.delete(idUser);
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while search the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
