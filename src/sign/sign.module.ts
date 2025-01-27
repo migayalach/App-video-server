e9e38f0fae769399d6cfd75ac3052d981d2374b8
migayalach <ayalachavezmiguel@gmail.com>
2025-01-27 16:54:56 -0400

    imports UserModule

--- src/sign/sign.module.ts
+++ src/sign/sign.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SignService } from './sign.service';
 import { SignController } from './sign.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [SignController],
   providers: [SignService],
 })
