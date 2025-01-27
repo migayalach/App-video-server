d353f61c4dbe96dde4e46781cd0335e9fa0023bd
migayalach <ayalachavezmiguel@gmail.com>
2025-01-27 16:50:32 -0400

    sign in dto

---
+++ src/sign/dto/signIn.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class SignInDto {
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'nicolasMiau@example.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'NIcomIAU*123@gmail.com',
+  })
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
