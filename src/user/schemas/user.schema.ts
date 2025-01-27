ac9032072060b1b9ef7342241ec3c9d4dcd746d3
migayalach <ayalachavezmiguel@gmail.com>
2025-01-27 16:55:53 -0400

    added new property 'follow'

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -12,6 +12,9 @@ export class User extends Document {
   @Prop({ required: true })
   password: string;
 
+  @Prop()
+  token?: string;
+
   @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }] })
   follow: Types.ObjectId[];
 }
