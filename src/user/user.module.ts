b595cde783ad3a14e6ef7e7d3620c9f3aeb85acc
migayalach <ayalachavezmiguel@gmail.com>
2025-01-27 16:54:34 -0400

    imports AuthModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   exports: [UserService],
