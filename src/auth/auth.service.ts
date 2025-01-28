b699c574a98878fdd98df5520a8cf67254836426
migayalach <ayalachavezmiguel@gmail.com>
2025-01-28 12:56:53 -0400

    auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { RefreshTokenDto } from './dto/refreshToken.dto';
+import { AuthToken } from './authToken';
+import { User } from 'src/user/schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class AuthService {
-  create(createRefreshTokenDto) {
-    return 'This action adds a new refreshToken';
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private authToken: AuthToken,
+  ) {}
+  async refreshToken(userInfo: RefreshTokenDto) {
+    const tokenInfo = await this.userModel
+      .findById(userInfo.idUser)
+      .select('-password');
+    if (userInfo.refreshToken !== tokenInfo.token) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: 'Sorry, this token is expired.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return await this.authToken.generateToken(
+      tokenInfo._id.toString(),
+      tokenInfo.name,
+    );
   }
 }
