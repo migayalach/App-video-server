bef1558c5ca9fced9c6b132b023029aa5ecb00a1
migayalach <ayalachavezmiguel@gmail.com>
2025-01-28 20:46:44 -0400

    config ready

--- src/seeders/initial-seeder.service.ts
+++ src/seeders/initial-seeder.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema';
 
 @Injectable()
 export class SeederService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async run() {
+  async seed() {
     const existingUsers = await this.userModel.countDocuments();
     if (existingUsers === 0) {
       const users = [
@@ -36,12 +36,12 @@ export class SeederService {
 
       try {
         await this.userModel.insertMany(users);
-        console.log('Datos iniciales cargados');
+        // console.log('Datos iniciales cargados');
       } catch (error) {
         console.error('Error al cargar los datos iniciales:', error);
       }
     } else {
-      console.log('Los datos ya están cargados');
+      // console.log('Los datos ya están cargados');
     }
   }
 }
