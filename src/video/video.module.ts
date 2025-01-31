084bb7d9d6b10e812a13ed8fba150680fbe0ea8d
migayalach <ayalachavezmiguel@gmail.com>
2025-01-30 23:53:48 -0400

    import ranking module

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -4,11 +4,13 @@ import { VideoController } from './video.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Video, VideoSchema } from './schemas/video.schema';
 import { UserModule } from 'src/user/user.module';
+import { RankingModule } from 'src/ranking/ranking.module';
 
 @Module({
   imports: [
-    UserModule,
     MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
+    UserModule,
+    RankingModule,
   ],
   exports: [VideoService],
   controllers: [VideoController],
