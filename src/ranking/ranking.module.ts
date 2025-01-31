2ac608729face9a613fb0a8934862e6a2b40997d
migayalach <ayalachavezmiguel@gmail.com>
2025-01-30 23:51:10 -0400

    import schema Ranking

--- src/ranking/ranking.module.ts
+++ src/ranking/ranking.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RankingService } from './ranking.service';
 import { RankingController } from './ranking.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Ranking, RankingSchema } from './schema/ranking.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Ranking.name, schema: RankingSchema }]),
+  ],
   controllers: [RankingController],
   providers: [RankingService],
+  exports: [RankingService],
 })
 export class RankingModule {}
