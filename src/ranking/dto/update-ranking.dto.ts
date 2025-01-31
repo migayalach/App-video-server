e70fdc786535d40ccf543e3942e10d150e7a13d9
migayalach <ayalachavezmiguel@gmail.com>
2025-01-30 23:47:02 -0400

    added property to create dto

--- src/ranking/dto/update-ranking.dto.ts
+++ src/ranking/dto/update-ranking.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateRankingDto } from './create-ranking.dto';
+import { Types } from 'mongoose';
+import { IsMongoId, IsNotEmpty, IsNumber, Max, Min } from 'class-validator';
+import { Transform } from 'class-transformer';
 
-export class UpdateRankingDto extends PartialType(CreateRankingDto) {}
+export class UpdateRankingDto extends PartialType(CreateRankingDto) {
+  @IsMongoId()
+  @IsNotEmpty()
+  idRanking: Types.ObjectId;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  idUser: Types.ObjectId;
+
+  @Transform(({ value }) => Number(value))
+  @Min(0)
+  @Max(5)
+  @IsNumber()
+  qualification: number;
+}
