56fb4fc8d9e9f08cdae5ad7a5d77faa80eef8f54
migayalach <ayalachavezmiguel@gmail.com>
2025-01-30 23:51:42 -0400

    create new method create and response success

--- src/ranking/ranking.service.ts
+++ src/ranking/ranking.service.ts
@@ -1,26 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateRankingDto } from './dto/create-ranking.dto';
 import { UpdateRankingDto } from './dto/update-ranking.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Ranking } from './schema/ranking.schema';
+import { Model, Types } from 'mongoose';
 
 @Injectable()
 export class RankingService {
-  create(createRankingDto: CreateRankingDto) {
-    return 'This action adds a new ranking';
+  constructor(
+    @InjectModel(Ranking.name) private rankingModel: Model<Ranking>,
+  ) {}
+  async create(infomation: CreateRankingDto) {
+    try {
+      const createRanking = new this.rankingModel({
+        idVideo: new Types.ObjectId(infomation.idVideo),
+      });
+      await createRanking.save();
+      return;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while creating the user.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all ranking`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ranking`;
-  }
-
-  update(id: number, updateRankingDto: UpdateRankingDto) {
-    return `This action updates a #${id} ranking`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} ranking`;
+  async update(id: string, infomation: UpdateRankingDto) {
+    return `This action updates a #${id} ${infomation}ranking`;
   }
 }
