987cfdf060f07fc710f1b9f15c8bcffd26912c16
migayalach <ayalachavezmiguel@gmail.com>
2025-01-30 21:43:16 -0400

    this file has two option download to mp3 and mp4 by quality

--- src/download/download.controller.ts
+++ src/download/download.controller.ts
@@ -1,34 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { DownloadService } from './download.service';
-import { CreateDownloadDto } from './dto/create-download.dto';
-import { UpdateDownloadDto } from './dto/update-download.dto';
 
 @Controller('download')
 export class DownloadController {
   constructor(private readonly downloadService: DownloadService) {}
 
-  @Post()
-  create(@Body() createDownloadDto: CreateDownloadDto) {
-    return this.downloadService.create(createDownloadDto);
-  }
-
   @Get()
-  findAll() {
-    return this.downloadService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.downloadService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDownloadDto: UpdateDownloadDto) {
-    return this.downloadService.update(+id, updateDownloadDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.downloadService.remove(+id);
+  async download(
+    @Query('url') url: string,
+    @Query('quality') quality: string,
+    @Query('format') format: string,
+  ) {
+    return await this.downloadService.downloadVideo(url, quality, format);
   }
 }
