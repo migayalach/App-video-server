7202ea1d74836c3b4038761d0004dc01eeb87749
migayalach <ayalachavezmiguel@gmail.com>
2025-01-31 23:44:00 -0400

    import MongoModule 'Ranking'

--- src/seeders/seed.module.module.ts
+++ src/seeders/seed.module.module.ts
@@ -4,11 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/user/schemas/user.schema';
 import { SeederCommand } from './seed.command';
 import { Video, VideoSchema } from 'src/video/schemas/video.schema';
+import { Ranking, RankingSchema } from 'src/ranking/schema/ranking.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
+    MongooseModule.forFeature([{ name: Ranking.name, schema: RankingSchema }]),
   ],
   providers: [SeederService, SeederCommand],
   exports: [SeederService],
