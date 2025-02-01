a47b322f5f91aa11e94852ee72175dcef81dc643
migayalach <ayalachavezmiguel@gmail.com>
2025-01-31 23:42:01 -0400

    added new methos 'findOne' by idVideo

--- src/ranking/ranking.service.ts
+++ src/ranking/ranking.service.ts
@@ -4,6 +4,7 @@ import { UpdateRankingDto } from './dto/update-ranking.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Ranking } from './schema/ranking.schema';
 import { Model, Types } from 'mongoose';
+import { totalAverange } from 'src/utils/operations.util';
 
 @Injectable()
 export class RankingService {
@@ -31,11 +32,80 @@ export class RankingService {
     }
   }
 
-  async findAll() {
-    return `This action returns all ranking`;
+  async findOne(idVideo: string) {
+    try {
+      const infoAverage = await this.rankingModel
+        .findOne({
+          idVideo: new Types.ObjectId(idVideo),
+        })
+        .select('_id average');
+      return infoAverage;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while updating the ranking.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  async update(id: string, infomation: UpdateRankingDto) {
-    return `This action updates a #${id} ${infomation}ranking`;
+  async update(idRanking: string, information: UpdateRankingDto) {
+    try {
+      const ranking = await this.rankingModel.findOneAndUpdate(
+        {
+          _id: idRanking,
+          'listVotes.idUser': information.idUser,
+        },
+        information.qualification === 0
+          ? { $pull: { listVotes: { idUser: information.idUser } } }
+          : {
+              $set: {
+                'listVotes.$.qualification': Number(information.qualification),
+              },
+            },
+        { new: true },
+      );
+
+      if (!ranking && information.qualification !== 0) {
+        await this.rankingModel.findByIdAndUpdate(idRanking, {
+          $push: {
+            listVotes: {
+              idUser: information.idUser,
+              qualification: information.qualification,
+            },
+          },
+        });
+      }
+
+      const updatedRanking = await this.rankingModel.findById(idRanking);
+      const average = totalAverange(updatedRanking.listVotes);
+
+      await this.rankingModel.findByIdAndUpdate(
+        idRanking,
+        { $set: { average } },
+        { new: true },
+      );
+
+      return {
+        message: 'Registered ranking.',
+        average,
+      };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'An unexpected error occurred while updating the ranking.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
