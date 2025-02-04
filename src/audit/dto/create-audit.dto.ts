e67384eb1d204811fc56ea2d730b97c91163cd74
migayalach <ayalachavezmiguel@gmail.com>
2025-02-03 23:44:39 -0400

    create dto for createAudit

--- src/audit/dto/create-audit.dto.ts
+++ src/audit/dto/create-audit.dto.ts
@@ -1,1 +1,26 @@
-export class CreateAuditDto {}
+import {
+  IsEnum,
+  IsMongoId,
+  IsNotEmpty,
+  IsObject,
+  IsOptional,
+} from 'class-validator';
+import { Types } from 'mongoose';
+import { AuditState } from 'src/enums/audit.enum';
+
+export class CreateAuditDto {
+  @IsMongoId()
+  @IsNotEmpty()
+  idUser: Types.ObjectId;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  idVideo: Types.ObjectId;
+
+  @IsEnum(AuditState)
+  action: AuditState;
+
+  @IsObject()
+  @IsOptional()
+  changes?: Record<string, any>;
+}
