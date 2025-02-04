a65dc49ca8b9ce9021b5f39f35af5f0442d88258
migayalach <ayalachavezmiguel@gmail.com>
2025-02-03 23:43:04 -0400

    imports MongooseModule 'Audit'and UserModule

--- src/audit/audit.module.ts
+++ src/audit/audit.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuditService } from './audit.service';
 import { AuditController } from './audit.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Audit, AuditSchema } from './schemas/audit.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Audit.name, schema: AuditSchema }]),
+    UserModule,
+  ],
   controllers: [AuditController],
   providers: [AuditService],
+  exports: [AuditService],
 })
 export class AuditModule {}
