743efdf8c741d75af87cd08cf7883a9aa44ff01a
migayalach <ayalachavezmiguel@gmail.com>
2025-02-03 23:40:55 -0400

    In process auditController with two methods Get and GetId

--- src/audit/audit.controller.ts
+++ src/audit/audit.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AuditService } from './audit.service';
 
 @Controller('audit')
 export class AuditController {
   constructor(private readonly auditService: AuditService) {}
 
-  @Get()
-  findAll() {
-    return this.auditService.findAll();
+  @Get('')
+  async findAll(
+    @Query('idUser') idUser: string,
+    @Query('page') page: string,
+  ): Promise<any> {
+    return await this.auditService.findAll(idUser, +page);
+  }
+
+  @Get(':idAudit')
+  async findOne(@Param('idAudit') idAudit: string): Promise<any> {
+    return await this.auditService.findOne(idAudit);
   }
 }
