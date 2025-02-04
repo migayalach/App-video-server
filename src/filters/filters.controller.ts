e1357786e9fd472088044e69c7d1d4efead261c4
migayalach <ayalachavezmiguel@gmail.com>
2025-02-03 23:47:32 -0400

    added new Query

--- src/filters/filters.controller.ts
+++ src/filters/filters.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { FiltersService } from './filters.service';
 @Controller('filters')
 export class FiltersController {
   constructor(private readonly filtersService: FiltersService) {}
 
   @Get()
-  findAll() {
-    return this.filtersService.findAll();
+  async findAll(
+    @Query('search') search: string,
+    @Query('data') data: any,
+    @Query('page') page: string,
+  ) {
+    return await this.filtersService.findAll(search, data, +page);
   }
 }
