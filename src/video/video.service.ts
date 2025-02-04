2d4d985c792b5990213e4b50c5532f23cf252391
migayalach <ayalachavezmiguel@gmail.com>
2025-02-03 23:49:24 -0400

    audit complete

--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -14,6 +14,9 @@ import {
   VideoResponseClear,
 } from 'src/interfaces/video.interface';
 import { Response } from 'src/interfaces/response.interface';
+import { AuditService } from 'src/audit/audit.service';
+import { AuditState } from 'src/enums/audit.enum';
+import { VideoDelete, VideoState } from 'src/enums/video.enum';
 
 @Injectable()
 export class VideoService {
@@ -21,15 +24,20 @@ export class VideoService {
     @InjectModel(Video.name) private videoModel: Model<Video>,
     private useSevice: UserService,
     private rankingService: RankingService,
+    private auditService: AuditService,
   ) {}
   async create(infoVideo: CreateVideoDto): Promise<VideoResponse> {
     try {
       await this.useSevice.findOne(infoVideo.idUser.toString());
-      if (await this.videoModel.findOne({ nameVideo: infoVideo.nameVideo })) {
+      if (
+        await this.videoModel.findOne({
+          $or: [{ nameVideo: infoVideo.nameVideo }, { url: infoVideo.url }],
+        })
+      ) {
         throw new HttpException(
           {
             status: HttpStatus.NOT_FOUND,
-            error: `Sorry this name currently exists`,
+            error: `Sorry this name or url currently exists.`,
           },
           HttpStatus.NOT_FOUND,
         );
@@ -40,9 +48,14 @@ export class VideoService {
       });
       const videoData = await createVideo.save();
       await this.rankingService.create({
+        idUser: new Types.ObjectId(videoData.idUser.toString()),
         idVideo: new Types.ObjectId(videoData._id.toString()),
       });
-
+      await this.auditService.create({
+        idUser: new Types.ObjectId(infoVideo.idUser),
+        idVideo: new Types.ObjectId(videoData._id.toString()),
+        action: AuditState.Create,
+      });
       return {
         message: 'Video created successfully',
         video: clearVideoRes(videoData),
@@ -54,14 +67,14 @@ export class VideoService {
       throw new HttpException(
         {
           status: HttpStatus.INTERNAL_SERVER_ERROR,
-          error: 'An unexpected error occurred while creating the user.',
+          error: 'An unexpected error occurred while creating the video.',
         },
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
 
-  async findAll(page?: number): Promise<Response> {
+  async findAll(page?: number): Promise<any> {
     try {
       const results = clearResVideos(
         await this.videoModel.find({ stateVideo: true }).select('-__v'),
@@ -70,6 +83,12 @@ export class VideoService {
         page = 1;
       }
 
+      for (let i = 0; i < results.length; i++) {
+        const { _id, average } = await this.rankingService.findOne(
+          results[i].idVideo,
+        );
+        results[i] = { ...results[i], idRanking: _id.toString(), average };
+      }
       return response(results, page, 'video?');
     } catch (error) {
       if (error instanceof HttpException) {
@@ -85,7 +104,7 @@ export class VideoService {
     }
   }
 
-  async findOne(idVideo: string): Promise<VideoResponseClear> {
+  async findOne(idVideo: string): Promise<any> {
     try {
       const videoInfo = await this.videoModel.findById(idVideo).select('-__v');
       if (!videoInfo) {
@@ -97,7 +116,9 @@ export class VideoService {
           HttpStatus.NOT_FOUND,
         );
       }
-      return clearVideoRes(videoInfo);
+      const clearInfo = clearVideoRes(videoInfo);
+      const { _id, average } = await this.rankingService.findOne(idVideo);
+      return { ...clearInfo, idRanking: _id.toString(), average };
     } catch (error) {
       if (error instanceof HttpException) {
         throw error;
@@ -112,12 +133,48 @@ export class VideoService {
     }
   }
 
-  async update(
-    idVideo: string,
-    infoVideo: UpdateVideoDto,
-  ): Promise<VideoResponse> {
+  async update(idVideo: string, infoVideo: UpdateVideoDto): Promise<any> {
     try {
-      await this.findOne(idVideo);
+      const videoData = await this.videoModel
+        .findById(idVideo)
+        .select('isDelete')
+        .lean();
+
+      if (videoData.isDelete === VideoDelete.true) {
+        throw new HttpException(
+          { status: HttpStatus.FORBIDDEN, message: 'Acceess denegate' },
+          HttpStatus.FORBIDDEN,
+        );
+      }
+      const changes: Record<string, any> = {};
+      const afterInfo = await this.findOne(idVideo);
+      for (const i in afterInfo) {
+        if (infoVideo.hasOwnProperty(i)) {
+          if (infoVideo[i] !== afterInfo[i]) {
+            changes[i] = {
+              after: afterInfo[i],
+              before: infoVideo[i],
+            };
+          }
+        }
+      }
+      if (!Object.keys(changes).length) {
+        throw new HttpException(
+          {
+            status: HttpStatus.NOT_FOUND,
+            message:
+              'We are sorry, we were unable to update the information because we do not have any new data.',
+          },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      await this.auditService.create({
+        idUser: new Types.ObjectId(infoVideo.idUser),
+        idVideo: new Types.ObjectId(idVideo),
+        action: AuditState.Update,
+        changes,
+      });
+
       const updateVideo = {
         ...infoVideo,
         idUser: new Types.ObjectId(infoVideo.idUser),
@@ -147,12 +204,32 @@ export class VideoService {
     }
   }
 
-  async remove(idVideo: string): Promise<VideoResponse> {
+  async remove(idVideo: string): Promise<any> {
     try {
-      await this.findOne(idVideo);
+      const { isDelete } = await this.findOne(idVideo);
+      if (isDelete === VideoDelete.true) {
+        throw new HttpException(
+          {
+            status: HttpStatus.NOT_FOUND,
+            message: "Sorry this video doesn't exist",
+          },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      const infoVideo = await this.videoModel.findByIdAndUpdate(idVideo, {
+        isDelete: true,
+      });
+
+      await this.useSevice.findOne(infoVideo.idUser.toString());
+
+      await this.auditService.create({
+        idUser: new Types.ObjectId(infoVideo.idUser),
+        idVideo: new Types.ObjectId(idVideo.toString()),
+        action: AuditState.Delete,
+      });
       return {
         message: `Video deleted successfully`,
-        video: clearVideoRes(await this.videoModel.findByIdAndDelete(idVideo)),
+        // video: clearVideoRes(await this.videoModel.findByIdAndDelete(idVideo)),
       };
     } catch (error) {
       if (error instanceof HttpException) {
