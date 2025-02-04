cfe8ea63b9edcfa481ce9b2422cd72f244a1274b
migayalach <ayalachavezmiguel@gmail.com>
2025-02-03 23:50:36 -0400

    import AuditModule

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -5,12 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Video, VideoSchema } from './schemas/video.schema';
 import { UserModule } from 'src/user/user.module';
 import { RankingModule } from 'src/ranking/ranking.module';
+import { AuditModule } from 'src/audit/audit.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
     UserModule,
     RankingModule,
+    AuditModule,
   ],
   exports: [VideoService],
   controllers: [VideoController],
