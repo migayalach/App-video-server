d4f483ae93421a01d2775edaa2d9613a4bfd79d2
migayalach <ayalachavezmiguel@gmail.com>
2025-02-03 23:55:07 -0400

    imports MongooseModule 'Audit', 'Video', 'User', 'Ranking'

--- src/filters/filters.module.ts
+++ src/filters/filters.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { FiltersService } from './filters.service';
 import { FiltersController } from './filters.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Audit, AuditSchema } from 'src/audit/schemas/audit.schema';
+import { Video, VideoSchema } from 'src/video/schemas/video.schema';
+import { User, UserSchema } from 'src/user/schemas/user.schema';
+import { Ranking, RankingSchema } from 'src/ranking/schema/ranking.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Audit.name, schema: AuditSchema }]),
+    MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: Ranking.name, schema: RankingSchema }]),
+  ],
   controllers: [FiltersController],
   providers: [FiltersService],
 })
