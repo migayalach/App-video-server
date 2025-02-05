f22bdb14d0f5b8e46539fb8837ab0e71311d6790
migayalach <ayalachavezmiguel@gmail.com>
2025-02-05 16:14:33 -0400

    import userModule

--- src/seeders/seed.module.module.ts
+++ src/seeders/seed.module.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SeederService } from './initial-seeder.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/user/schemas/user.schema';
 import { SeederCommand } from './seed.command';
-import { Video, VideoSchema } from 'src/video/schemas/video.schema';
-import { Ranking, RankingSchema } from 'src/ranking/schema/ranking.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
-    MongooseModule.forFeature([{ name: Ranking.name, schema: RankingSchema }]),
-  ],
+  imports: [UserModule],
   providers: [SeederService, SeederCommand],
   exports: [SeederService],
 })
