894b44429df30d34e3904b9df331448ff442de18
migayalach <ayalachavezmiguel@gmail.com>
2025-02-05 21:12:30 -0400

    import like module

--- src/seeders/seed.module.module.ts
+++ src/seeders/seed.module.module.ts
@@ -4,9 +4,10 @@ import { SeederCommand } from './seed.command';
 import { UserModule } from 'src/user/user.module';
 import { VideoModule } from 'src/video/video.module';
 import { FollowModule } from 'src/follow/follow.module';
+import { LikeModule } from 'src/like/like.module';
 
 @Module({
-  imports: [UserModule, VideoModule, FollowModule],
+  imports: [UserModule, VideoModule, FollowModule, LikeModule],
   providers: [SeederService, SeederCommand],
   exports: [SeederService],
 })
