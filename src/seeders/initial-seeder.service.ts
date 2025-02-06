1f7fbfb16626fe073be3c32118146a1a84638ec9
migayalach <ayalachavezmiguel@gmail.com>
2025-02-05 20:27:05 -0400

    follow created with success

--- src/seeders/initial-seeder.service.ts
+++ src/seeders/initial-seeder.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { listUsers, listVideos } from '../helpers/initialData.helper';
 import { VideoService } from 'src/video/video.service';
+import { FollowService } from 'src/follow/follow.service';
+import { Follow } from 'src/enums/follow.enum';
 
 @Injectable()
 export class SeederService {
   constructor(
     private userService: UserService,
     private videoService: VideoService,
+    private followService: FollowService,
   ) {}
+
+  // private name(params: any) {}
 
   async seed() {
     if (!(await this.userService.databaseSize())) {
       try {
+        console.log('*****************************************');
         const addedUsers = await Promise.all(
           listUsers.map(async (data) => {
             const { user } = await this.userService.create(data);
@@ -20,6 +26,7 @@ export class SeederService {
           }),
         );
 
+        console.log('*****************************************');
         let count = 0;
         let key = 0;
         for (let i = listUsers.length; i > 0; i--) {
@@ -34,6 +41,26 @@ export class SeederService {
           count = 0;
         }
 
+        console.log('*****************************************');
+        let index = 1;
+        let limit = listUsers.length;
+        let current = 0;
+        while (current < listUsers.length) {
+          if (index !== limit) {
+            for (let j = index; j < limit; j++) {
+              await this.followService.create({
+                idUser: addedUsers[current].idUser,
+                idCreador: addedUsers[j].idUser,
+                option: Follow.Adding,
+              });
+            }
+            index++;
+            current++;
+          } else {
+            index = 0;
+            limit--;
+          }
+        }
         // console.log('Datos iniciales cargados');
       } catch (error) {
         console.error('Error al cargar los datos iniciales:', error);
