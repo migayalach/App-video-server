6d8e3b23fa0e46bcd49da8b862c6d29446d9d3b8
migayalach <ayalachavezmiguel@gmail.com>
2025-02-05 21:56:10 -0400

    seeders ready

--- src/seeders/initial-seeder.service.ts
+++ src/seeders/initial-seeder.service.ts
@@ -5,6 +5,7 @@ import { VideoService } from 'src/video/video.service';
 import { FollowService } from 'src/follow/follow.service';
 import { Follow } from 'src/enums/follow.enum';
 import { LikeService } from 'src/like/like.service';
+import { RankingService } from 'src/ranking/ranking.service';
 
 @Injectable()
 export class SeederService {
@@ -13,80 +14,97 @@ export class SeederService {
     private videoService: VideoService,
     private followService: FollowService,
     private likeService: LikeService,
+    private rankingService: RankingService,
   ) {}
 
-  // private name(params: any) {}
+  private async createUsers() {
+    return Promise.all(
+      listUsers.map(async (data) => {
+        const { user } = await this.userService.create(data);
+        return user;
+      }),
+    );
+  }
+
+  private async createVideos(addedUsers: any[]) {
+    let count = 0;
+    let key = 0;
+    const videoList = [];
+
+    for (let i = listUsers.length; i > 0; i--) {
+      while (count < i) {
+        const {
+          video: { idVideo },
+        } = await this.videoService.create({
+          ...listVideos[key],
+          idUser: addedUsers[i - 1].idUser,
+        });
+        videoList.push(idVideo);
+        key++;
+        count++;
+      }
+      count = 0;
+    }
+    return videoList;
+  }
+
+  private async createFollows(addedUsers: any[]) {
+    let index = 1;
+    let limit = addedUsers.length;
+    let current = 0;
+
+    while (current < addedUsers.length) {
+      if (index !== limit) {
+        for (let j = index; j < limit; j++) {
+          await this.followService.create({
+            idUser: addedUsers[current].idUser,
+            idCreador: addedUsers[j].idUser,
+            option: Follow.Adding,
+          });
+        }
+        index++;
+        current++;
+      } else {
+        index = 0;
+        limit--;
+      }
+    }
+  }
+
+  private async createLikesAndRankings(addedUsers: any[], videoList: any[]) {
+    for (let i = 0; i < videoList.length; i++) {
+      const limit = addedUsers.length + addedUsers.length / 2;
+      const number = Math.floor(Math.random() * limit);
+
+      if (number < addedUsers.length) {
+        await this.likeService.create({
+          idUser: addedUsers[number].idUser,
+          idVideo: videoList[i],
+        });
+
+        const { idRanking } = await this.videoService.findOne(videoList[i]);
+        const information = {
+          idUser: addedUsers[number].idUser,
+          qualification: Math.floor(Math.random() * 5),
+        };
+        await this.rankingService.update(idRanking, information);
+      }
+    }
+  }
 
   async seed() {
     if (!(await this.userService.databaseSize())) {
       try {
-        console.log('*****************************************');
-        const addedUsers = await Promise.all(
-          listUsers.map(async (data) => {
-            const { user } = await this.userService.create(data);
-            return user;
-          }),
-        );
-
-        console.log('*****************************************');
-        let count = 0;
-        let key = 0;
-        const videoList = [];
-        for (let i = listUsers.length; i > 0; i--) {
-          while (count < i) {
-            const {
-              video: { idVideo },
-            } = await this.videoService.create({
-              ...listVideos[key],
-              idUser: addedUsers[i - 1].idUser,
-            });
-            videoList.push(idVideo);
-            key++;
-            count++;
-          }
-          count = 0;
-        }
-
-        console.log('*****************************************');
-        let index = 1;
-        let limit = listUsers.length;
-        let current = 0;
-        while (current < listUsers.length) {
-          if (index !== limit) {
-            for (let j = index; j < limit; j++) {
-              await this.followService.create({
-                idUser: addedUsers[current].idUser,
-                idCreador: addedUsers[j].idUser,
-                option: Follow.Adding,
-              });
-            }
-            index++;
-            current++;
-          } else {
-            index = 0;
-            limit--;
-          }
-        }
-
-        console.log('*****************************************');
-        for (let i = 0; i < videoList.length; i++) {
-          const limit = addedUsers.length + addedUsers.length / 2;
-          const number = Math.floor(Math.random() * limit);
-          if (number < addedUsers.length) {
-            await this.likeService.create({
-              idUser: addedUsers[number].idUser,
-              idVideo: videoList[i],
-            });
-          }
-        }
-
-        console.log('*****************************************');
-        // console.log('Datos iniciales cargados');
+        const addedUsers = await this.createUsers();
+        const videoList = await this.createVideos(addedUsers);
+        await this.createFollows(addedUsers);
+        await this.createLikesAndRankings(addedUsers, videoList);
+        console.log('Datos iniciales cargados');
       } catch (error) {
         console.error('Error al cargar los datos iniciales:', error);
       }
     } else {
-      // console.log('Los datos ya están cargados');
+      console.log('Los datos ya están cargados');
     }
   }
 }
