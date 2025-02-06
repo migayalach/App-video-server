5ca8f22152b0cfae7208206bfbcd1576bf996334
migayalach <ayalachavezmiguel@gmail.com>
2025-02-05 20:26:22 -0400

    import followModule

--- src/seeders/seed.module.module.ts
+++ src/seeders/seed.module.module.ts
@@ -3,9 +3,10 @@ import { SeederService } from './initial-seeder.service';
 import { SeederCommand } from './seed.command';
 import { UserModule } from 'src/user/user.module';
 import { VideoModule } from 'src/video/video.module';
+import { FollowModule } from 'src/follow/follow.module';
 
 @Module({
-  imports: [UserModule, VideoModule],
+  imports: [UserModule, VideoModule, FollowModule],
   providers: [SeederService, SeederCommand],
   exports: [SeederService],
 })
