83ab60aa45ae7bcc5072a4319c49f39d8f683999
migayalach <ayalachavezmiguel@gmail.com>
2025-02-07 21:23:08 -0400

    sign ready to use

--- src/sign/sign.service.ts
+++ src/sign/sign.service.ts
@@ -2,12 +2,17 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { SignUpDto } from './dto/signUp.dto';
 import { SignInDto } from './dto/signIn.dto';
 import { UserService } from 'src/user/user.service';
+import {
+  ExistEmail,
+  UserCreated,
+  UserDelete,
+} from 'src/interfaces/user.interface';
 
 @Injectable()
 export class SignService {
   constructor(private userService: UserService) {}
 
-  async create(infoUser: SignUpDto) {
+  async create(infoUser: SignUpDto): Promise<UserCreated> {
     try {
       return await this.userService.create(infoUser);
     } catch (error) {
@@ -24,7 +29,7 @@ export class SignService {
     }
   }
 
-  async update(infoUser: SignInDto) {
+  async update(infoUser: SignInDto): Promise<ExistEmail> {
     try {
       return await this.userService.emailExist(
         infoUser.email,
@@ -37,16 +42,19 @@ export class SignService {
       throw new HttpException(
         {
           status: HttpStatus.INTERNAL_SERVER_ERROR,
-          error: 'An unexpected error occurred while search the user.',
+          error: 'An unexpected error occurred while searching for the user.',
         },
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
 
-  async delete(idUser: string) {
+  async delete(idUser: string): Promise<UserDelete> {
     try {
-      return await this.userService.delete(idUser);
+      await this.userService.delete(idUser);
+      return {
+        message: 'Bye',
+      };
     } catch (error) {
       if (error instanceof HttpException) {
         throw error;
@@ -54,7 +62,7 @@ export class SignService {
       throw new HttpException(
         {
           status: HttpStatus.INTERNAL_SERVER_ERROR,
-          error: 'An unexpected error occurred while search the user.',
+          error: 'An unexpected error occurred while logging out.',
         },
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
