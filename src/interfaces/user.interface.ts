781d257c8ac7afd1fbb4ba5d49b0656ecad802f6
migayalach <ayalachavezmiguel@gmail.com>
2025-02-07 21:21:56 -0400

    added new interfaces as TokenUser UserCreated UserUpdate ExistEmail CreatorData

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,12 +1,37 @@
 import { ObjectId } from 'mongoose';
 
-export interface UserResponse {
+export interface UserDelete {
   message: string;
-  user: UserData;
 }
 
 export interface UserData {
   idUser: string | ObjectId;
   name: string;
   email: string;
+  picture: string;
+  follow: Array<CreatorData> | [];
+}
+
+export interface TokenUser {
+  access_token: string;
+  refresh_token: string;
+}
+
+export interface UserCreated {
+  message: string;
+  user: UserData;
+  token: TokenUser;
+}
+
+export interface UserUpdate {
+  message: string;
+  user: UserData;
+}
+
+export interface ExistEmail extends UserData, TokenUser {}
+
+export interface CreatorData {
+  idCreator: string;
+  nameCreator: string;
+  pictureCreator: string;
 }
