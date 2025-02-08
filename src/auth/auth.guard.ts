12129032194439f39eb6d398e93d439fd219f504
migayalach <ayalachavezmiguel@gmail.com>
2025-02-07 21:27:25 -0400

    new guard whit new logic

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -15,19 +15,29 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
+
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('No token provided');
     }
+
     try {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret,
       });
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
+
+      // Aquí comparamos el idUser del token con el idUser del parámetro de la ruta
+      const userIdFromToken = payload.sub; // O la propiedad que uses en el token
+      const userIdFromParams = request.params.idUser;
+
+      if (userIdFromToken !== userIdFromParams) {
+        throw new UnauthorizedException('Invalid token for this user');
+      }
+
       request['user'] = payload;
-    } catch {
-      throw new UnauthorizedException();
+    } catch (error) {
+      throw new UnauthorizedException(`Invalid or expired token: ${error}`);
     }
+
     return true;
   }
 
