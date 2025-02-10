e66cf4bcd13fb0492170af0274c873cf630784e5
migayalach <ayalachavezmiguel@gmail.com>
2025-02-09 23:58:20 -0400

    added new property

--- src/auth/dto/refreshToken.dto.ts
+++ src/auth/dto/refreshToken.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class RefreshTokenDto {
+  @ApiProperty({
+    description: 'The "id" of the user',
+    example: '678726a888a21c3097b5c5d1',
+  })
   @IsString()
   @IsNotEmpty()
   idUser: string;
 
+  @ApiProperty({
+    description: 'The token access of the user',
+    example: '123aaa456.xxx789zzz.mmmnnn1010',
+  })
   @IsString()
   refreshToken: string;
 }
