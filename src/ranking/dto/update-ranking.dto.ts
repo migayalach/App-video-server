2fa079ca7e7a4802584c58ddbe98b8d50e50f6e1
migayalach <ayalachavezmiguel@gmail.com>
2025-02-10 23:56:54 -0400

    update dto ranking

--- src/ranking/dto/update-ranking.dto.ts
+++ src/ranking/dto/update-ranking.dto.ts
@@ -3,12 +3,26 @@ import { Types } from 'mongoose';
 import { IsMongoId, IsNotEmpty, IsNumber, Max, Min } from 'class-validator';
 import { Transform } from 'class-transformer';
 import { CreateRankingDto } from './create-ranking.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateRankingDto extends PartialType(CreateRankingDto) {
+  @ApiProperty({
+    description: 'Unique identifier of the user who is uploading the user.',
+    example: '6787c139d67c1c00e1758f88',
+    type: String,
+  })
   @IsMongoId()
   @IsNotEmpty()
   idUser: Types.ObjectId;
 
+  @ApiProperty({
+    description:
+      'User qualification for the ranking, should be between 0 and 5.',
+    example: 4,
+    type: Number,
+    minimum: 0,
+    maximum: 5,
+  })
   @Transform(({ value }) => Number(value))
   @Min(0)
   @Max(5)
