032511785226e40740c4a8512d4fb4ae74d822e2
migayalach <ayalachavezmiguel@gmail.com>
2025-02-11 00:04:08 -0400

    time 15min for token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '45m' },
+      signOptions: { expiresIn: '15m' },
     }),
   ],
   controllers: [AuthController],
