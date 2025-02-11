0d7a50caa8df1595dcb2e34280bafc00bded7572
migayalach <ayalachavezmiguel@gmail.com>
2025-02-10 23:51:29 -0400

    created three enum news

---
+++ src/enums/download.enum.ts
@@ -0,0 +1,18 @@
+export enum VideoQuality {
+  '144p',
+  '360p',
+  '480p',
+  '720p',
+  '1080p',
+}
+
+export enum MP3Quality {
+  low = 'low',
+  high = 'high',
+  best = 'best',
+}
+
+export enum DownloadFormat {
+  'mp4',
+  'mp3',
+}
