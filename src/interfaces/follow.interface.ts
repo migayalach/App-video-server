826d809037e6341fb0f02817b989bc158bb5340b
migayalach <ayalachavezmiguel@gmail.com>
2025-02-10 23:59:02 -0400

    added new interface

--- src/interfaces/follow.interface.ts
+++ src/interfaces/follow.interface.ts
@@ -1,6 +1,13 @@
-import { UserData } from './user.interface';
+import { ObjectId } from 'mongoose';
+
+export interface ChannelUser {
+  idUser: string | ObjectId;
+  name: string;
+  email: string;
+  picture: string;
+}
 
 export interface FollowResponse {
   message: string;
-  creator: UserData;
+  creator: ChannelUser;
 }
