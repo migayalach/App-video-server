d7b28336cd437386e4e747b76b81fe8a669c4fe3
migayalach <ayalachavezmiguel@gmail.com>
2025-02-10 23:57:07 -0400

    created dto ranking

--- src/ranking/dto/create-ranking.dto.ts
+++ src/ranking/dto/create-ranking.dto.ts
@@ -1,11 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsMongoId, IsNotEmpty } from 'class-validator';
 import { Types } from 'mongoose';
 
 export class CreateRankingDto {
+  @ApiProperty({
+    description: 'Unique identifier of the user who is uploading the user.',
+    example: '6787c139d67c1c00e1758f88',
+    type: String,
+  })
   @IsMongoId()
   @IsNotEmpty()
   idUser: Types.ObjectId;
 
+  @ApiProperty({
+    description: 'Unique identifier of the user who is uploading the video.',
+    example: '6787c139d67c1c00e1758f88',
+    type: String,
+  })
   @IsMongoId()
   @IsNotEmpty()
   idVideo: Types.ObjectId;
