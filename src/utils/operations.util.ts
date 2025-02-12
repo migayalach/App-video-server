d1049092d3a254867f724ad7eae887f4c8f333ad
migayalach <ayalachavezmiguel@gmail.com>
2025-02-11 23:50:08 -0400

    added if becase listVotes is 0 return average iquals to 0

--- src/utils/operations.util.ts
+++ src/utils/operations.util.ts
@@ -1,5 +1,8 @@
 export const totalAverange = (listVotes) => {
   let average = 0;
+  if (!listVotes.length) {
+    return average;
+  }
   for (let i = 0; i < listVotes.length; i++) {
     average += listVotes[i].qualification;
   }
