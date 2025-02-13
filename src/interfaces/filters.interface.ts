61f341ba3f504aec54acff289a8ef7437f731bec
migayalach <ayalachavezmiguel@gmail.com>
2025-02-12 21:53:47 -0400

    added two new interface for filters

--- src/interfaces/filters.interface.ts
+++ src/interfaces/filters.interface.ts
@@ -1,3 +1,6 @@
+import { ObjectId } from 'mongoose';
+import { AuditState } from 'src/enums/audit.enum';
+import { OrderFilters } from 'src/enums/filters.enum';
 import { VideoDelete } from 'src/enums/video.enum';
 
 export interface VideoQuery {
@@ -6,4 +9,19 @@ export interface VideoQuery {
     $gte?: Date;
     $lte?: Date;
   };
+}
+
+export interface OptionsFilters {
+  idUser?: string | ObjectId;
+  action?: AuditState;
+  dateStart?: Date;
+  dateEnd?: Date;
+  order?: OrderFilters;
+  key?: string;
+}
+
+export interface QueryAudit {
+  idUser: string | ObjectId;
+  action?: AuditState;
+  timeChanges?: string | { $gte?: Date; $lte?: Date };
 }
