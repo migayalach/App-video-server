9852ffb3396c292cd045297bf24bd9eec5d409de
migayalach <ayalachavezmiguel@gmail.com>
2025-02-28 17:49:36 -0400

    delete mongoose package

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { LikeController } from './like.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Like, LikeSchema } from './schemas/like.schema';
 import { UserModule } from 'src/user/user.module';
 import { VideoModule } from 'src/video/video.module';
 
 @Module({
-  imports: [
-    UserModule,
-    VideoModule,
-    MongooseModule.forFeature([{ name: Like.name, schema: LikeSchema }]),
-  ],
+  imports: [UserModule, VideoModule],
   controllers: [LikeController],
   providers: [LikeService],
   exports: [LikeService],
