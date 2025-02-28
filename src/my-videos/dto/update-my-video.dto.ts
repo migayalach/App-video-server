098f3fff9e2edc89ddc59f6dd5bc36b294bedac2
migayalach <ayalachavezmiguel@gmail.com>
2025-02-28 18:11:41 -0400

    update info and delete files

--- src/my-videos/dto/update-my-video.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMyVideoDto } from './create-my-video.dto';
-
-export class UpdateMyVideoDto extends PartialType(CreateMyVideoDto) {}
--- src/my-videos/my-videos.service.ts
+++ src/my-videos/my-videos.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MyVideosService {
   async findOne(idUser: string) {
-    return `This action returns a #${idUser} myVideo`;
+    try {
+      return `This action returns a #${idUser} myVideo`;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error:
+            'An unexpected error occurred while searching for your favorite videos.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
