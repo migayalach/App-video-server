645b88db576b48fef687942ff4eb1c296bbedf35
migayalach <ayalachavezmiguel@gmail.com>
2025-03-04 18:02:31 -0400

    imported userModele and videoModule

--- src/my-videos/my-videos.module.ts
+++ src/my-videos/my-videos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MyVideosService } from './my-videos.service';
 import { MyVideosController } from './my-videos.controller';
+import { VideoModule } from 'src/video/video.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UserModule, VideoModule],
   controllers: [MyVideosController],
   providers: [MyVideosService],
 })
